0333f00e5face3710df5754afe53c7e651a5f60c
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-16 16:08:52 -0700

    Fix: flake overlay

--- flake.nix
+++ flake.nix
@@ -5,18 +5,30 @@
 
   inputs.utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
+  outputs = { self, nixpkgs, utils }:
     {
       overlay = final: prev: {
         buildGoApplication = final.callPackage ./builder { };
         gomod2nix = final.callPackage ./default.nix { };
       };
-
-      defaultPackage = pkgs.callPackage ./default.nix { };
-
-    });
-
+    } //
+    (utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlay
+            ];
+          };
+        in
+        {
+          defaultPackage = pkgs.callPackage ./default.nix { };
+          devShell = with pkgs; mkShell {
+            buildInputs = [
+              gomod2nix
+            ];
+          };
+        })
+    );
 }
