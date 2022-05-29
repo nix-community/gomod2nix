ce3a6771f0a019a804d068c8192161b23f7aefc2
adisbladis <adisbladis@gmail.com>
2022-05-29 17:06:13 +0800

    gomod2nix: Filter builder expressions from cli build

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ buildGoApplication {
   pname = "gomod2nix";
   version = "0.1";
   src = lib.cleanSourceWith {
-    filter = name: type: ! lib.hasSuffix "tests" name;
+    filter = name: type: (! lib.hasSuffix "tests" name) && (! lib.hasSuffix "builder" name);
     src = lib.cleanSource ./.;
   };
   modules = ./gomod2nix.toml;
@@ -18,6 +18,5 @@ buildGoApplication {
 
   postInstall = ''
     wrapProgram $out/bin/gomod2nix --prefix PATH : ${lib.makeBinPath [ go ]}
-    rm -f $out/bin/builder
   '';
 }
