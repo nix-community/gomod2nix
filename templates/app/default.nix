759483247eab2ceee46ff64a3e73daa2a908e016
adisbladis <adisbladis@gmail.com>
2022-06-14 17:35:19 +0800

    Align flake template with recent API changes and improvements

--- templates/app/default.nix
+++ templates/app/default.nix
@@ -1,10 +1,20 @@
-{ buildGoApplication, go, lib }:
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+}:
 
-buildGoApplication {
-  inherit go;
+pkgs.buildGoApplication {
   pname = "myapp";
   version = "0.1";
+  pwd = ./.;
   src = ./.;
   modules = ./gomod2nix.toml;
-  subPackages = [ "." ];
 }
