5913cf95d5e802b44c9674ce7049b748361b1683
adisbladis <adisbladis@gmail.com>
2022-06-14 18:56:12 +0800

    buildGoApplication: add support for checkFlags
    This is a port of https://github.com/NixOS/nixpkgs/commit/dc973241a9c836f4207f2949331df0ee0a3abdf2

--- builder/default.nix
+++ builder/default.nix
@@ -322,7 +322,7 @@ let
           runHook preCheck
 
           for pkg in $(getGoDirs test); do
-            buildGoDir test "$pkg"
+            buildGoDir test $checkFlags "$pkg"
           done
 
           runHook postCheck
