c3f52f3d79ffdd8aa858f7c5f366def8f2216ba2
Sarah Brofeldt <sarah@qtr.dk>
2022-08-31 09:55:27 +0200

    overlay: fallback to default callPackage for stable nixpkgs

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,8 @@
 final: prev:
 let
-  callPackage = final.darwin.apple_sdk_11_0.callPackage;
+  # The newer Darwin SDK does not exist in current (nixos-22.05) stable
+  # branches, so just fallback to the default callPackage.
+  callPackage = final.darwin.apple_sdk_11_0.callPackage or final.callPackage;
 in
 {
   inherit (callPackage ./builder { }) buildGoApplication mkGoEnv;
