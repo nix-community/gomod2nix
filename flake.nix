3f884fa2bf494aae014d1e8e8b07265cc698c35e
Alex Jackson <contact@ajaxbits.com>
2024-07-29 10:48:15 -0500

    Fix: add back darwin support
    Fixes a regression where darwin support was deleted. We now take the
    full default value of `defaultSystems` from flake-utils and add riscv to
    it.

--- flake.nix
+++ flake.nix
@@ -19,8 +19,10 @@
     }
     // (flake-utils.lib.eachSystem
       [
-        "x86_64-linux"
         "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
         "riscv64-linux"
       ]
       (
