bf376bd322ec9c84f6bec75d78aa0b3544cc8049
Marcus Ramberg <marcus@means.no>
2025-08-22 14:19:56 +0200

    fix: get rid of removed darwin sdk, bump flake

--- templates/app/flake.nix
+++ templates/app/flake.nix
@@ -21,7 +21,7 @@
 
         # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
         # This has no effect on other platforms.
-        callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
+        callPackage = pkgs.callPackage;
         # Simple test check added to nix flake check
         go-test = pkgs.stdenvNoCC.mkDerivation {
           name = "go-test";
